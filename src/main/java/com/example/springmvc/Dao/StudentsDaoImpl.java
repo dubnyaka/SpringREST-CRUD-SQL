cf584b0dd1bf4c5390d574886b6f65072b5cb72b
dubnyaka <dubnyaka@gmail.com>
2021-11-29 13:27:48 +0200

    Removed static from Dao

--- src/main/java/com/example/springmvc/Dao/StudentsDaoImpl.java
+++ src/main/java/com/example/springmvc/Dao/StudentsDaoImpl.java
@@ -13,10 +13,10 @@ import java.util.concurrent.atomic.AtomicLong;
 @Repository
 public class StudentsDaoImpl implements StudentsDao {
     // Хранилище студентов
-    private static final Map<Long, Student> STUDENT_REPOSITORY_MAP = new ConcurrentHashMap<>();
+    private final Map<Long, Student> STUDENT_REPOSITORY_MAP = new ConcurrentHashMap<>();
 
     // Переменная для генерации ID студентов
-    private static final AtomicLong STUDENT_ID_HOLDER = new AtomicLong();
+    private final AtomicLong STUDENT_ID_HOLDER = new AtomicLong();
 
     @Override
     public void save(Student student) {
